18decef80865a983112576b3b5379b80df31a247
denisshchurin <denisshchurin@gmail.com>
2019-04-20 18:16:27 +0200

    add getter and setter in class Book

--- src/main/java/com/denis/bookmanager/model/Book.java
+++ src/main/java/com/denis/bookmanager/model/Book.java
@@ -5,4 +5,36 @@ public class Book {
     private String title;
     private String author;
     private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
